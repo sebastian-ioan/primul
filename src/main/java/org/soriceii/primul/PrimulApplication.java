a790e1df9f290bcbc7ad67330068e35e3eff444e
Sebastian IOAN <sebastian.ioan@gmail.com>
2019-08-10 20:10:47 +0300

    Test.

---
+++ src/main/java/org/soriceii/primul/PrimulApplication.java
@@ -0,0 +1,13 @@
+package org.soriceii.primul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/soriceii/primul/PrimulApplicationTests.java
@@ -0,0 +1,16 @@
+package org.soriceii.primul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
